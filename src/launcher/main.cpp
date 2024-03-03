84a94d514f2a4921f5022888cbeb46d597c5fdcb
Goodbird <goodbirdmail@yandex.ru>
2024-03-03 11:51:48 +0300

    Removed boost dependency, added test directory to CMake project

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -1,13 +1,8 @@
-#include <boost/program_options.hpp>
 #include <string>
 #include <vector>
-//#include <algorithm>
-//#include <iterator>
 #include <iostream>
 #include <fstream>
-
-using namespace boost::program_options;
-
+#include <sstream>
 // Начальные установки параметров компилятора и его запуск
 void Compile(const char* str);
 
@@ -17,64 +12,23 @@ void to_cout(const std::vector<std::string> &v)
     std::ostream_iterator<std::string>{std::cout, "\n"});
 }
 
+void print_help() {
+    std::cout << "Help screen\n";
+    std::cout << "Options:\n";
+    std::cout << "Use --file fileName to specify the file to compile\n";
+}
+
 int main(int argc, const char *argv[])
 {
-    // Распознавание опций командной строки, обеспечивающих выбор
-    // каталога проекта и компилируемой сущности (артефакта)
-    std::string packageDir;     // каталог пакета содержащего компилируемую сущность
-    std::string fileName;         // компилируемая сущность
-
-    try {
-        options_description desc{"Options"};
-        desc.add_options()
-            ("help,h", "Help screen")
-//            ("package,p", value(&packageDir), "Package")
-/*            ("entity,e", value<std::vector<std::string>>()->
-                multitoken()->zero_tokens()->composing(), "Compiled entity");
-*/
-            ("file,f", value(&fileName), "File with module");
-
-        // Ограничение позиционных аргументов (возможны без опции) одним
-        positional_options_description pos_desc;
-        //pos_desc.add("phone", -1);
-        pos_desc.add("file", 1);
-
-        command_line_parser parser{argc, argv};
-        parser.options(desc).positional(pos_desc).allow_unregistered();
-        parsed_options parsed_options = parser.run();
-
-        variables_map vm;
-        store(parsed_options, vm);
-        notify(vm);
-
-        // Вывод подсказки
-        if (vm.count("help")) {
-            if(argc > 2) {
-                std::cout << "Use only one option --help or -h for help\n";
-                return 0;
-            }
-            std::cout << desc << '\n';
-            return 0;
-        }
-    
-        // Вывод непозиционных параметров (в нашем случае он пока один)
-        if (vm.count("file")) {
-            /*
-            to_cout(vm["entity"].as<std::vector<std::string>>());
-            std::cout << "Entity is: " << vm["entity"].as<std::vector<std::string>>()[0] << "\n";
-            */
-            std::cout << "file is: " << fileName << "\n";
-        }
-
-/*
-        if (vm.count("package")) {
-            std::cout << "Package is: " << packageDir << '\n';
-            //return 0;
-        }
-*/
+    std::string fileName;
+        
+    if (argc != 3 || strcmp(argv[1], "--file")!=0) {
+        print_help();
+        return 0;
     }
-    catch (const error &ex) {
-        std::cerr << ex.what() << '\n';
+    else {
+        fileName = argv[2];
+        std::cout << "file is: " << fileName << "\n";
     }
     
     // После получения имен открытие и чтение файла с модулем
@@ -91,8 +45,9 @@ int main(int argc, const char *argv[])
         std::cout << "Module" << fileName << " isn't accesible" << std::endl;
         return 1;
     }
+
     std::stringstream ss;
-    ss << moduleStream.rdbuf();
+    ss << (moduleStream.rdbuf());
     std::string o7module(ss.str()); // Модуль Оберона 7
     /// Тестовый вывод прочитанного артефакта
     std::cout << o7module << std::endl;
