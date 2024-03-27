bb8238af442b5463806d5c06c32084562e2e8998
Vasilevskiy Vladimir <vvil1568@yandex.ru>
2024-03-27 17:07:55 +0300

    Добавлен стек контекста для генерации правильно названных меток

--- src/codegen/codegencontext.h
+++ src/codegen/codegencontext.h
@@ -3,7 +3,18 @@
 
 #include <vector>
 #include <string>
+#include <stack>
 
+//Структура описывает кадр стека контекста
+struct ContextStackFrame {
+	std::string name;
+	int ifCount = 0;
+	int caseCount = 0;
+	int forCount = 0;
+	int whileCount = 0;
+};
+
+//Контекст кодогенератора
 class CodeGenContext {
 public:
 	static CodeGenContext& getInstance() {
@@ -21,12 +32,47 @@ public:
 		getInstance().strLitCount++;
 		return "literal_str_" + std::to_string(getInstance().strLitCount);
 	}
-
+	static std::string pushContext(std::string name) {
+		if (name == "if" || name == "case" || name == "for" || name == "while") {
+			ContextStackFrame top = getInstance().contextStack.back();
+			if (name == "if") {
+				top.ifCount++;
+				name += std::to_string(top.ifCount);
+			}
+			if (name == "case") {
+				top.caseCount++;
+				name += std::to_string(top.ifCount);
+			}
+			if (name == "for") {
+				top.forCount++;
+				name += std::to_string(top.ifCount);
+			}
+			if (name == "while") {
+				top.whileCount++;
+				name += std::to_string(top.ifCount);
+			}
+		}
+		getInstance().contextStack.push_back(ContextStackFrame { name, 0, 0, 0, 0 });
+		std::string labelName = "";
+		bool first = true;
+		for (auto it = getInstance().contextStack.begin(); it != getInstance().contextStack.end(); it++) {
+			if (!first) {
+				labelName += "_";
+			}
+			first = false;
+			labelName += (*it).name;
+		}
+		return labelName;
+	}
+	static void popContext() {
+		getInstance().contextStack.pop_back();
+	}
 	std::vector<std::string> code;
 	std::vector<std::string> data;
 	std::string dataIndent;
 	std::string codeIndent;
 private:
+	std::vector<ContextStackFrame> contextStack;
 	int strLitCount = 0;
 	CodeGenContext();
 };
